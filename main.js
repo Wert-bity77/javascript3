c8584e8e6da6943e9003886a26ec9bc4c22f4b10
Wert-bity77 <kabulovt707@gmail.com>
2024-11-20 01:02:14 +0500

    main js
    sssssss

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let random = Math.floor(Math.random()* 10)
+let  keyser = alert ('Sizda hozir random sonlar chiqishni boshleydi👌')
+let  keyser2 = alert (`Random orqali chiqqan son : ${random}    ` )
+let  keyser3 = alert ('biz random sonni 2 ga  kopaytirmoqchimiz va bolmoqchimiz')
+alert(`Random orqali chiqqan son: ${random} '\n'   biz uni 2 ga kopaytirib: ${random*2} soni chiqardik   '\n'  va  shu sonni 2 ga bolib:  ${random/2} soni chiqardik '\n'  va shu sonni  3ga bolgandagi qoldiq:  ${random*2}  sonini chiqardik`)
+
+
+
+
+
+
